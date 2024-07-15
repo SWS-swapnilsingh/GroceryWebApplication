4df568374fc40869bb2ca6bd0e5245c19340fe2b
swapnil singh <113300936+SWS-swapnilsingh@users.noreply.github.com>
2024-07-15 08:18:41 +0530

    Update celeryconfig.py

--- Code/celeryconfig.py
+++ Code/celeryconfig.py
@@ -1,4 +1,10 @@
-broker_url = "redis://localhost:6379/1"
-result_backend = "redis://localhost:6379/2"
+# broker_url = "rediss://red-cqa4hnlds78s739m96ag:tJtilMCBDMxhZsrMkeYsAchXhmsq0dFb@oregon-redis.render.com:6379/1"
+# result_backend = "rediss://red-cqa4hnlds78s739m96ag:tJtilMCBDMxhZsrMkeYsAchXhmsq0dFb@oregon-redis.render.com:6379/2"
+# timezone = 'Asia/Kolkata'
+# broker_connection_retry_on_startup=True
+
+#deployment celeryconfig
+broker_url = "redis://red-cqa4hnlds78s739m96ag:6379/1"
+result_backend = "redis://red-cqa4hnlds78s739m96ag:6379/2"
 timezone = 'Asia/Kolkata'
-broker_connection_retry_on_startup=True
+broker_connection_retry_on_startup = True
