2fe2772a11067d9e0ef60ee506348d9580fbb822
swapnil singh <113300936+SWS-swapnilsingh@users.noreply.github.com>
2024-07-15 22:26:01 +0530

    add code to add categories and products and the time of initialization of database upload_initial_data.py

--- Code/upload_initial_data.py
+++ Code/upload_initial_data.py
@@ -4,6 +4,10 @@ from application.models import db, Role
 from flask_security import hash_password
 from werkzeug.security import generate_password_hash
 
+#not part of original code
+from application.models import db, Category, Category_product, Product
+
+
 
 #agar koi cheej hai jisko run karne ke liye application level data chahiye to use humlogo ko app context ke andar run karna padega otherwise error aayegi
 #in our case we need database uri which is application level data
@@ -45,6 +49,31 @@ if run_first_time:
             datastore.create_user(username="Manager1", email="mgr1@email.com", password=generate_password_hash("Mm1@"), roles=["manager"], active=False)
         if not datastore.find_user(email="user1@email.com"):
             datastore.create_user(username="ram", email="user1@email.com", password=generate_password_hash("Uu1@"), roles=["normal_user"])
+
+            for cname in ['Lunch', 'Dinner', 'BreakFast', 'Fruits']:
+                nc1 = Category(category_name = cname, is_approved = True)
+                db.session.add(nc1)
+                db.session.commit()
+        
+            p_tuple_list = [(1, 'Rice', 10, 'kg', '12-12-2024', '13-12-2024', 10, 0, 2),
+                        (2, 'Salad', 15, 'pc', '12-12-2024', '13-12-2024', 10, 0, 2),
+                        (2, 'Curry', 70, 'kg', '12-12-2024', '13-12-2024', 3, 5, 2),
+                        (3, 'Milk', 50, 'lt', '12-12-2024', '13-12-2024', 10, 0, 2),
+                        (1, 'Chocolate', 20, 'lt', '12-12-2024', '13-12-2024', 10, 10, 2),
+                        (1, 'Idli', 18, 'kg', '12-12-2024', '13-12-2024', 10, 20, 2)]
+            for (categoryId, product_name, price_unit, unit, manufac_year, exp_year, qty, discount, mgr_id) in p_tuple_list:
+                 #adding new product to the product table
+                p1 = Product(product_name = product_name, price_unit=price_unit, unit=unit, manufac_date = manufac_year, exp_date = exp_year, quantity=qty, discount=discount,mgr_id=mgr_id)
+                db.session.add(p1)
+                db.session.commit()
+    
+                #adding category-product relation inside categoryProduct table
+                # c1 = Category.query.get(categoryId)
+                p1_ = Product.query.filter(Product.product_name == product_name).first()
+    
+                cp1 = Category_product(cp_category_id = categoryId, cp_product_id = p1_.product_id)
+                db.session.add(cp1)
+                db.session.commit()
         run_first_time = False
         
         db.session.commit()
